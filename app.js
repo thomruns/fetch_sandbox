f151d43185164ca2145764f02732d88f63fe06d7
Thom David <thomdavid74@gmail.com>
2018-03-28 15:44:36 -0700

    Convert to arrow functions

--- app.js
+++ app.js
@@ -5,51 +5,80 @@ document.getElementById('button3').addEventListener('click', getExternal);
 // Get local text file data
 function getText() {
   fetch('data_files/textdata.txt')
-  .then(function(res) {
-    return res.text();  //RETURNS A PROMISE
-  })
-  .then(function(data) {
+  .then(res => res.text())
+  .then(data => {
     document.getElementById('output').innerHTML = data;
   })
-  .catch(function(err) {
-    console.log(err);
-  });
+  .catch(err => console.log(err));
 }
 
 // Get local JSON data
 function getJson() {
   fetch('data_files/posts.json')
-  .then(function(res) {
-    return res.json();  //RETURNS A PROMISE
-  })
-  .then(function(data) {
-    console.log(data);
+  .then(res => res.json())
+  .then(data => {
+    //console.log(data);
     let output = '';
-    data.forEach(function(post) {
+    data.forEach(post => {
       output += `<li>${post.title}</li>`;
     });
     document.getElementById('output').innerHTML = output;
   })
-  .catch(function(err) {
-    console.log(err);
-  });
+  .catch(err => console.log(err));
 }
 
 // Get external API data
 function getExternal() {
   fetch('https://api.github.com/users')
-  .then(function(res) {
-    return res.json();  //RETURNS A PROMISE
-  })
-  .then(function(data) {
-    console.log(data);
+  .then(res => res.json())
+  .then(data => {
+    //console.log(data);
     let output = '';
-    data.forEach(function(user) {
+    data.forEach(user => {
       output += `<li>${user.login}</li>`;
     });
     document.getElementById('output').innerHTML = output;
   })
-  .catch(function(err) {
-    console.log(err);
-  });
+  .catch(err => console.log(err));
 }
+
+// Simple arrow functions
+// const sayHello = function() {
+//   console.log('Hello');
+// }
+
+// sayHello();
+// One line function does not neet braces
+// const sayHello = () => console.log('Hello again');
+// sayHello();
+
+// One line returns
+// const sayGoodbye = () => 'Goodbye';
+// console.log(sayGoodbye());
+
+// Return object literal, wrap in parens
+// const sayGoodbye = () => ({ msg: 'Goodbye'});
+// console.log(sayGoodbye());
+
+// Single parameter doesn't require parens
+// const sayHello = name => console.log(`Hello, ${ name }`);
+
+// Multiple parameters require parens
+// const sayHello = (firstName, lastName) => console.log(`Hello, ${ firstName } ${ lastName }`);
+
+// sayHello('John', 'Doe');
+
+// const users = ['Bill', 'Joe', 'William'];
+// // const nameLengths = users.map(function(name) {
+// //   return name.length;
+// // });
+
+// // Shorter
+// // const nameLengths = users.map((name) => {
+// //   return name.length;
+// // });
+
+// // Shortest
+// const nameLengths = users.map(name => name.length);
+
+// console.log(nameLengths);
